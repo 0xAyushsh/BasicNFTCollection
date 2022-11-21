cbee829d16103fe5a201d55596b027eb5950b56b
Lucifer <this.ayushsh@gmail.com>
2022-11-22 03:37:54 +0530

    Constants changed

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,6 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x31A8C3133C8e4B88F273C7E15ad044ECaCD3d1fF";
 
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://basic-nft-collection.vercel.app//api/";
+const METADATA_URL = "https://basic-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };

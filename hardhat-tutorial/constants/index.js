e2e26af7f2988c9e3382a608f0f4b8544d4ee8e6
Lucifer <this.ayushsh@gmail.com>
2022-11-22 03:14:51 +0530

    NFT Collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract
+const WHITELIST_CONTRACT_ADDRESS = "0x31A8C3133C8e4B88F273C7E15ad044ECaCD3d1fF";
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  // Address of the whitelist contract
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

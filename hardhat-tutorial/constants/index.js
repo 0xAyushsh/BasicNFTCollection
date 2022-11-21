00547de91bf9c2eaaa7c12ba548d61ba901cd2cc
Lucifer <this.ayushsh@gmail.com>
2022-11-22 03:43:39 +0530

    Constants changed

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,9 @@
 // Address of the Whitelist Contract
 const WHITELIST_CONTRACT_ADDRESS = "0x31A8C3133C8e4B88F273C7E15ad044ECaCD3d1fF";
 
-// URL to extract Metadata for a Crypto Dev NFT
+//MetaData URL
 const METADATA_URL = "https://basic-nft-collection.vercel.app/api/";
+
+
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
